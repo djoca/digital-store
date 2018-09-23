9d0d7eba30221a57910aec3bf5d52764752141fd
Jose Antonio Rosa dos Santos Junior <joses.jr@gmail.com>
2018-09-23 09:48:32 -0300

    Initial configuration

---
+++ src/main/java/net/j33r/digitalstore/Boot.java
@@ -0,0 +1,12 @@
+package net.j33r.digitalstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(Boot.class, args);
+    }
+}
